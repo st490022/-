c01286d2bc9411780efb3add178e6919df3e34ea
st490022 <st490022@skjhs.ntct.edu.tw>
2026-08-18 12:30:59 +0800

    Update predict.py

--- scripts/predict.py
+++ scripts/predict.py
@@ -10,7 +10,7 @@ def run_prediction(historical_data):
     model.fit(df)
     
     # 3. 預測未來 24 小時
-    future = model.make_future_dataframe(periods=24, freq='H')
+    future = model.make_future_dataframe(periods=24, freq='h')
     forecast = model.predict(future)
     
     # 回傳預測結果 (包含時間 ds 與 預測值 yhat)
